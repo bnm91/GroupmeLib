b2b4bda10e58e7c357472335103b12ffc86e741a
bnm91 <ball.nixonm@gmail.com>
2017-05-12 09:32:40 -0400

    adding first pass at setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+packages = [
+    'GroupmeLib'
+]
+
+setup(
+    name='decker',
+    version='0.0.1',
+    description='Groupme library for python',
+    author='Nixon Ball',
+    packages=packages
+    include_package_data=True,
+    install_requires=[
+        'requests>=2.8.1'
+    ],
+)
