58e8b84825c4c7b1ebc36f26173dceabfa04206f
bnm91 <ball.nixonm@gmail.com>
2017-05-12 17:11:45 -0400

    added several analysis functions

--- analysis.py
+++ analysis.py
@@ -1,6 +1,55 @@
 from groupmeLib import messages
 
 #TODO refactor
+#TODO create util module/package
+#TODO create util function that takes an output that uses user_id and prints it with Name
+
+
+def getSelfFavoritedCountByUser(groupId, msgs=None, groupUserDictionary=None):
+    if(msgs is None):
+        msgs = messages.getAllGroupMessages(groupId)
+    if(groupUserDictionary is None):
+        groupUserDictionary = getAllGroupUsers(groupId, msgs)
+
+    selfFavoritesCounts = {}
+    for message in msgs:
+        for favoriter in message['favorited_by']:
+            if(groupUserDictionary[favoriter] not in selfFavoritesCounts.keys()):
+                selfFavoritesCounts[groupUserDictionary[favoriter]] = 0
+            if(message['user_id'] == favoriter):
+                selfFavoritesCounts[groupUserDictionary[favoriter]] += 1
+    return selfFavoritesCounts
+
+
+#TODO: refactor this so is done by user_id instead of Name
+def getFavoritedCountByUser(groupId, msgs=None, groupUserDictionary=None):
+    if(msgs is None):
+        msgs = messages.getAllGroupMessages(groupId)
+    if(groupUserDictionary is None):
+        groupUserDictionary = getAllGroupUsers(groupId, msgs)
+
+    favoriteCounts = {}
+    for message in msgs:
+        if(groupUserDictionary[message['user_id']] not in favoriteCounts.keys()):
+            favoriteCounts[groupUserDictionary[message['user_id']]] = 0
+        favoriteCounts[groupUserDictionary[message['user_id']]] += len(message['favorited_by'])
+    return favoriteCounts
+
+
+#TODO: refactor this so is done by user_id instead of Name
+def getFavoritesGivenCountbyUser(groupId, msgs=None, groupUserDictionary=None):
+    if(msgs is None):
+        msgs = messages.getAllGroupMessages(groupId)
+    if(groupUserDictionary is None):
+        groupUserDictionary = getAllGroupUsers(groupId, msgs)
+
+    favoritesGivenCounts = {}
+    for message in msgs:
+        for favoriter in message['favorited_by']:
+            if(groupUserDictionary[favoriter] not in favoritesGivenCounts.keys()):
+                favoritesGivenCounts[groupUserDictionary[favoriter]] = 0
+            favoritesGivenCounts[groupUserDictionary[favoriter]] += 1
+    return favoritesGivenCounts
 
 
 def getAllGroupUsers(groupId, msgs=None):
@@ -35,7 +84,7 @@ def getMessageCountPerUser(groupId, msgs=None, groupUserDictionary=None):
 
 
 msgs = messages.getAllGroupMessages(5954413)
-print getMessageCountPerUser(5954413, msgs)
+print getSelfFavoritedCountByUser(5954413, msgs)
 
 #(13104384) SPORTS
 #(30425709) Brewgaloo
--- groupmeLib/__init__.py
+++ groupmeLib/__init__.py
@@ -1,2 +1,6 @@
 #TODO: consider refactoring down to fewer commands that take parameter variable
 #TODO: consider refactoring so that lowest level is really just pure api wrapper and move stuff like "GroupDictionary" up a level
+
+#TODO refactor
+#TODO create util module/package
+#TODO create util function that takes an output that uses user_id and prints it with Name
